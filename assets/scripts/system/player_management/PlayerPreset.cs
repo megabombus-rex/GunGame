ae5b99953d261ed9452c78c6f91f2c5da3a675ba
megabombus-rex <czechumennakompie@gmail.com>
2025-10-06 16:44:12 +0200

    added player presets

---
+++ assets/scripts/system/player_management/PlayerPreset.cs
@@ -0,0 +1,9 @@
+namespace GunGame.assets.scripts.system.player_management
+{
+    public struct PlayerPreset
+    {
+        public PlayerMovementMapping MovementMapping { get; init; }
+        public PlayerDisplayAndPhysics DisplayAndPhysics { get; init; }
+        public PlayerStats Stats { get; init; }
+    }
+}
---
+++ assets/scripts/system/player_management/PlayerStats.cs
@@ -0,0 +1,11 @@
+namespace GunGame.assets.scripts.system.player_management
+{
+    public struct PlayerStats
+    {
+        public int MaxSpeed {  get; set; }
+        public float Acceleration { get; set; }
+        public float JumpForce { get; set; }
+        public float Mass { get; set; }
+        public float GroundRayLength { get; set; }
+    }
+}

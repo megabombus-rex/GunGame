3362dd85c031c04c60b96540277ca3a95e70e2bc
megabombus-rex <czechumennakompie@gmail.com>
2025-10-09 10:44:37 +0200

    unified Vector2 usage

--- assets/scripts/misc/ShapeCreator.cs
+++ assets/scripts/misc/ShapeCreator.cs
@@ -6,12 +6,12 @@ namespace GunGame.assets.scripts.misc
 
     public static class ShapeCreator
     {
-        public static Shape2D GetShapeBasedOnShapeType(ShapeType type, System.Numerics.Vector2 shapeDetails)
+        public static Shape2D GetShapeBasedOnShapeType(ShapeType type, Vector2 shapeDetails)
         {
             return type switch
             {
                 ShapeType.Circle => new CircleShape2D() { Radius = shapeDetails.X },
-                ShapeType.Rectangle => new RectangleShape2D() { Size = new Godot.Vector2(shapeDetails.X, shapeDetails.Y) },
+                ShapeType.Rectangle => new RectangleShape2D() { Size = new Vector2(shapeDetails.X, shapeDetails.Y) },
                 ShapeType.Capsule => new CapsuleShape2D() { Radius = shapeDetails.X, Height = shapeDetails.Y },
                 _ => throw new NotImplementedException("Shape not implemented.")
             };
